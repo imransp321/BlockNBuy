e42663330b69a9e96b64ed5d0140816d29a93c7c
Nagaveni Papaiah <nagaveni.papaiah@volvo.com>
2020-06-06 21:17:09 +0530

    'SafeBuy

---
+++ ASP.NET MVC Application with a Custom Layout/C#/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult DashboardV1()
+        {
+            return View();
+        }
+        public ActionResult DashboardV2()
+        {
+            return View();
+        }
+        public ActionResult BookSlot()
+        {
+            return View();
+        }
+        public ActionResult ConfirmSlot()
+        {
+            return View();
+        }
+    }
+}
